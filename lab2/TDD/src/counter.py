d60d251e940e4049dfcaa2414bf64e0bface88fe
David <lam.david459@yahoo.com>
2024-09-20 10:18:52 -0700

    Make flake8 happy

--- lab2/TDD/src/counter.py
+++ lab2/TDD/src/counter.py
@@ -4,16 +4,18 @@ from src import status
 app = Flask(__name__)
 COUNTERS = {}
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message" : f"Counter{name} already exists."}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter{name} already exists."}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
 
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
@@ -21,10 +23,11 @@ def update_counter(name):
 
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message" : f"Counter {name} does not exist."}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist."}, status.HTTP_404_NOT_FOUND
 
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
@@ -32,6 +35,6 @@ def get_counter(name):
 
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message" : f"Counter {name} does not exist."}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} does not exist."}, status.HTTP_404_NOT_FOUND
 
-    return {name : COUNTERS[name]}, status.HTTP_200_OK
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
